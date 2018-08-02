066690cb6d45e3d36ed1fe9d5c2919527e289c61
jhanagustave <jhana.gustave@gmail.com>
2018-08-02 10:41:11 -0700

    mise  a jour pour week 4

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
@@ -0,0 +1,6 @@
+package com.codepath.apps.restclienttemplate.fragments;
+
+import android.support.v4.app.Fragment;
+
+public class TweetsListFragment extends Fragment {
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsPagerAdapter.java
@@ -0,0 +1,6 @@
+package com.codepath.apps.restclienttemplate.fragments;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TweetsPagerAdapter extends FragmentPagerAdapter {
+}
